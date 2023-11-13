82b24a2a98ce77cd0bc2d295754c6a9a79b4840f
Baurzhan Yussupov <baurzhangulnur@gmail.com>
2023-11-14 00:19:19 +0600

    updated base URL of database

--- src/axios-dishes.js
+++ src/axios-dishes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://restaurant-dishes-672d6-default-rtdb.firebaseio.com"
+    baseURL: "https://restaurant-dishes-d51eb-default-rtdb.firebaseio.com"
 });
 
 export default instance;
